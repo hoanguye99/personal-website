0872415f6361088280a58a4b476bc066ab369791
Hoang Nguyen <hdnguyen0305@gmail.com>
2024-09-19 10:57:04 +0700

    font size design system

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -27,291 +27,74 @@ const config: Config = {
       },
     },
     fontSize: {
-      'heading-0': [
-        '64px',
+      'heading-0-sm': [
+        '2.25rem',
         {
-          lineHeight: '65px',
-          fontWeight: '400',
+          lineHeight: '2.5rem',
+          fontWeight: '700',
+        },
+      ],
+      'heading-0': [
+        '3rem',
+        {
+          lineHeight: '1',
+          fontWeight: '700',
+        },
+      ],
+      'heading-1-sm': [
+        '1.125rem',
+        {
+          lineHeight: '1.75rem',
+          fontWeight: '500',
         },
       ],
       'heading-1': [
-        '56px',
+        '1.25rem',
         {
-          lineHeight: '65px',
-          fontWeight: '600',
+          lineHeight: '1.75rem',
+          fontWeight: '500',
         },
       ],
       'heading-2': [
-        '46px',
+        '0.875rem',
         {
-          lineHeight: '56px',
-          fontWeight: '600',
+          lineHeight: '1.25rem',
+          fontWeight: '700',
         },
       ],
-      'heading-3': [
-        '38px',
+      title: [
+        '1rem',
         {
-          lineHeight: '46px',
-          fontWeight: '600',
-        },
-      ],
-      'heading-4': [
-        '30px',
-        {
-          lineHeight: '38px',
-          fontWeight: '600',
-        },
-      ],
-      'heading-5': [
-        '24px',
-        {
-          lineHeight: '32px',
-          fontWeight: '600',
-        },
-      ],
-      'heading-6': [
-        '20px',
-        {
-          lineHeight: '28px',
-          fontWeight: '600',
-        },
-      ],
-      'heading-7': [
-        '16px',
-        {
-          lineHeight: '24px',
-          fontWeight: '600',
-        },
-      ],
-      'heading-8': [
-        '14px',
-        {
-          lineHeight: '22px',
-          fontWeight: '600',
-        },
-      ],
-      'heading-9': [
-        '12px',
-        {
-          lineHeight: '20px',
-          fontWeight: '600',
-        },
-      ],
-      'title-large': [
-        '38px',
-        {
-          lineHeight: '48px',
+          lineHeight: '1.375',
           fontWeight: '500',
         },
       ],
-      'title-1': [
-        '24px',
+      // 'subtitle': [
+      //   '18px',
+      //   {
+      //     lineHeight: '26px',
+      //     fontWeight: '400',
+      //   },
+      // ],
+      // 'body-1': [
+      //   '18px',
+      //   {
+      //     lineHeight: '28px',
+      //     fontWeight: '400',
+      //   },
+      // ],
+      body: [
+        '0.875rem',
         {
-          lineHeight: '32px',
+          lineHeight: '1.5',
+          fontWeight: '400',
+        },
+      ],
+      tag: [
+        '0.875rem',
+        {
+          lineHeight: '1.25rem',
           fontWeight: '500',
-        },
-      ],
-      'title-2': [
-        '20px',
-        {
-          lineHeight: '28px',
-          fontWeight: '500',
-        },
-      ],
-      'title-3': [
-        '16px',
-        {
-          lineHeight: '24px',
-          fontWeight: '500',
-        },
-      ],
-      'title-4': [
-        '16px',
-        {
-          lineHeight: '22px',
-          fontWeight: '500',
-        },
-      ],
-      'title-5': [
-        '14px',
-        {
-          lineHeight: '22px',
-          fontWeight: '500',
-        },
-      ],
-      'title-6': [
-        '14px',
-        {
-          lineHeight: '22px',
-          fontWeight: '400',
-        },
-      ],
-      'body-1': [
-        '18px',
-        {
-          lineHeight: '28px',
-          fontWeight: '400',
-        },
-      ],
-      'body-2': [
-        '16px',
-        {
-          lineHeight: '24px',
-          fontWeight: '400',
-        },
-      ],
-      'body-3': [
-        '14px',
-        {
-          lineHeight: '22px',
-          fontWeight: '400',
-        },
-      ],
-      'button-1': [
-        '16px',
-        {
-          lineHeight: '24px',
-          fontWeight: '600',
-        },
-      ],
-      'button-2': [
-        '16px',
-        {
-          lineHeight: '24px',
-          fontWeight: '500',
-        },
-      ],
-      'button-3': [
-        '14px',
-        {
-          lineHeight: '22px',
-          fontWeight: '500',
-        },
-      ],
-      'button-4': [
-        '14px',
-        {
-          lineHeight: '22px',
-          fontWeight: '400',
-        },
-      ],
-      'button-5': [
-        '12px',
-        {
-          lineHeight: '16px',
-          fontWeight: '500',
-        },
-      ],
-      'button-6': [
-        '12px',
-        {
-          lineHeight: '16px',
-          fontWeight: '400',
-        },
-      ],
-      'subtitle-1': [
-        '18px',
-        {
-          lineHeight: '26px',
-          fontWeight: '400',
-        },
-      ],
-      'subtitle-2': [
-        '16px',
-        {
-          lineHeight: '24px',
-          fontWeight: '400',
-        },
-      ],
-      'subtitle-3': [
-        '14px',
-        {
-          lineHeight: '22px',
-          fontWeight: '400',
-        },
-      ],
-      'subtitle-4': [
-        '12px',
-        {
-          lineHeight: '16px',
-          fontWeight: '400',
-        },
-      ],
-      'subtitle-5': [
-        '10px',
-        {
-          lineHeight: '14px',
-          fontWeight: '400',
-        },
-      ],
-      'label-1': [
-        '16px',
-        {
-          lineHeight: '24px',
-          fontWeight: '500',
-        },
-      ],
-      'label-2': [
-        '16px',
-        {
-          lineHeight: '22px',
-          fontWeight: '400',
-        },
-      ],
-      'label-3': [
-        '14px',
-        {
-          lineHeight: '22px',
-          fontWeight: '500',
-        },
-      ],
-      'label-4': [
-        '14px',
-        {
-          fontWeight: '400',
-          lineHeight: '22px',
-        },
-      ],
-      'label-5': [
-        '12px',
-        {
-          lineHeight: '16px',
-          fontWeight: '400',
-        },
-      ],
-      'placeholder-1': [
-        '16px',
-        {
-          lineHeight: '28px',
-          fontWeight: '400',
-        },
-      ],
-      'placeholder-2': [
-        '14px',
-        {
-          lineHeight: '22px',
-          fontWeight: '400',
-        },
-      ],
-      'caption-1': [
-        '14px',
-        {
-          lineHeight: '22px',
-          fontWeight: '400',
-        },
-      ],
-      'caption-2': [
-        '12px',
-        {
-          lineHeight: '16px',
-          fontWeight: '400',
-        },
-      ],
-      'caption-3': [
-        '10px',
-        {
-          lineHeight: '14px',
-          fontWeight: '400',
         },
       ],
     },
