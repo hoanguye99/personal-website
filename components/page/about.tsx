a8657a931ffde01a443b72c1906429969329f0af
Hoang Nguyen <hdnguyen0305@gmail.com>
2024-09-27 15:11:48 +0700

    contents

--- components/page/about.tsx
+++ components/page/about.tsx
@@ -8,24 +8,22 @@ const AboutSection = () => {
       <SectionHeader title="ABOUT" />
       <div className="flex flex-col gap-4">
         <p>
-          Back in 2012, I decided to try my hand at creating custom Tumblr
-          themes and tumbled head first into the rabbit hole of coding and web
-          development. Fast-forward to today, and I’ve had the privilege of
-          building software for an advertising agency, a start-up, a huge
-          corporation, and a digital product studio.
+          Hello! My name is Hoang and I like creating things that live on the
+          internet. I enjoy building software in the sweet spot where design and
+          engineering meet — things that look good but are also built well under
+          the hood.
         </p>
         <p>
-          My main focus these days is building accessible user interfaces for
-          our customers at Klaviyo. I most enjoy building software in the sweet
-          spot where design and engineering meet — things that look good but are
-          also built well under the hood. In my free time, I've also released an
-          online video course that covers everything you need to know to build a
-          web app with the Spotify API.
+          I spend a great deal of time tinkering with design systems, as I
+          firmly believe that a high-quality UI is built on high-quality
+          components. Components should not only be visually appealing but also
+          interactive, with smooth and fluid transitions to enhance the user
+          experience.
         </p>
         <p>
-          When I’m not at the computer, I’m usually rock climbing, reading,
-          hanging out with my wife and two cats, or running around Hyrule
-          searching for Korok seeds K o r o k s e e d s .
+          I am currently seeking a new opportunity where I can contribute to a
+          product-focused team and collaborate in an English-speaking
+          environment
         </p>
         <div className="grid gap-5 lg:grid-cols-4 lg:mt-5">
           <p className="-mt-[2px]">Recent techstack:</p>
