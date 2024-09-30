da10f71bff0a20cb5f4031748548ca7194a2bf1e
Hoang Nguyen <hdnguyen0305@gmail.com>
2024-09-30 08:34:57 +0700

    add sample OG

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,23 @@ const inter = localFont({
 export const metadata: Metadata = {
   title: 'Hoang Nguyen',
   description: 'Hoang Nguyen Personal Website',
+  openGraph: {
+    title: 'Hoang Nguyen',
+    description: 'Hoang Nguyen Personal Website',
+    url: 'https://hoanguye99.vercel.app/',
+    siteName: 'Hoang Nguyen Personal Website',
+    type: 'website',
+    images: [
+      {
+        url: 'https://raw.githubusercontent.com/gitdagray/my-blogposts/main/images/og-card.png',
+        secureUrl:
+          'https://raw.githubusercontent.com/gitdagray/my-blogposts/main/images/og-card.png',
+        width: 1200,
+        height: 630,
+        alt: 'Preview image for Hoang Nguyen',
+      },
+    ],
+  },
 }
 
 export default function RootLayout({
